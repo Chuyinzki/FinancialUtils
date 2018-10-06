e13219e2e5e716a8da0c669d6a7d287fbee2c3f3
Jesus Villegas <jesus_villegas@intuit.com>
2018-10-05 18:01:20 -0700

    Initial files to calc

---
+++ src/MACRS.java
@@ -0,0 +1,12 @@
+public class MACRS {
+
+    final double[] sevenYear = {14.29, 24.49, 17.49, 12.49, 8.93, 8.92, 8.93, 4.46};
+
+    public static void main(String[] args) {
+
+    }
+
+    static void printSchedule(final double initialValue, double[] percentages) {
+
+    }
+}
