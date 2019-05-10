9cf5a680f4b19f39d141fc076359e7a2f769078d
Jesus Villegas <jesus_villegas@intuit.com>
2019-05-10 10:05:10 -0700

    Also adding them in order. Crazy how it all actually does add up.

--- src/main/java/util/StatementAggregator.java
+++ src/main/java/util/StatementAggregator.java
@@ -1,10 +1,12 @@
-package main.java.util;
+package util;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class StatementAggregator {
@@ -14,18 +16,29 @@ public class StatementAggregator {
         BufferedReader buf = new BufferedReader(new InputStreamReader(is));
         String line = buf.readLine();
         int counter = 1;
-        while(line != null){
+        List<Double> charges = new ArrayList<>();
+        while (line != null) {
             StringTokenizer tokenizer = new StringTokenizer(line);
-            while(tokenizer.hasMoreTokens()) {
+            while (tokenizer.hasMoreTokens()) {
                 String token = tokenizer.nextToken();
-                if(token.charAt(0) == '$') {
+                if (token.charAt(0) == '$') {
                     Double toAdd = Double.parseDouble(token.substring(1));
+                    charges.add(toAdd);
                     System.out.println(String.format("%d: $%.2f", counter++, toAdd));
                     result += toAdd;
                 }
             }
             line = buf.readLine();
         }
+
+        Collections.sort(charges);
+        int counter2 = 1;
+        System.out.println("Here they are sorted:");
+
+        for (Double doubly : charges)
+            System.out.println(String.format("%d: $%.2f", counter++, doubly));
+
+
         System.out.println(String.format("The total amount for this statement is: $%.2f", result));
     }
 }
